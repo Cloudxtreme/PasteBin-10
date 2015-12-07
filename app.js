227861ad9b206e7250ffa45b7e1b42d213afff61
John <crumatoc@gmail.com>
2015-12-06 21:33:50 -0500

    Basically had to redo the initial layout. Couldn't bother with depreciation

--- app.js
+++ app.js
@@ -1,37 +1,16 @@
 //Declare dependencies
 var path = require('path');
 var express = require('express');
-var mongoose = require ('mongoose');
 var bodyParser = require('body-parser');
-var session = require('express-session');
-var cookieParser = require('cookie-parser');
-var pages = require('./controllers/pages.js');
-var views = require('./controllers/views.js');
-var create = require('./controllers/create.js');
+var router = express.Router();
 var app = express();
 
-//Configure spme app uses
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use('/api', pages);
-app.use('/', [views, create]);
-app.use(cookieParser());
-app.use(session({ resave: true, saveUninitialized: true, secret: 'PastebinCloneSecret' }));
+router.get('/', function(req, res)
+{
+	res.json({"message": "Han shot first."});
+});
 
-app.set('view engine', 'jade');
-app.set('views', path.join(__dirname, '/views'));
-app.use('/public', express.static(__dirname + '/public'));
+app.use('/', router);
 
-app.set('view engine', 'jade');
-app.use('/public', express.static(__dirname + '/../public'));
-
-//Declare database connections
-var dbName = 'Paste';
-var connectionString = 'mongodb://localhost:27017/' + dbName;
-
-//Mongoose/databasing
-mongoose.connect(connectionString);
-
-module.exports = app;
-
-//ROUTING IS IN /CONTROLLERS
+app.listen(8080);
+console.log("Listneing on port 8080")
--- bin/www.js
+++
@@ -1,9 +0,0 @@
-//Require app.js in root
-var app = require('../app');
-
-//Set port variable
-var port = process.env.PORT || 8080;
-
-//Start server
-app.listen(port);
-console.log('Server listening on ' + port);
